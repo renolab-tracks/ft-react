1955d2fa61dd1fa88475427631232d1761dfc86f
Ehssan ElAtfy <ehssan.elatfy@reno.systems>
2023-06-11 21:46:30 +0300

    fix merge conflict

--- src/Layout.js
+++ src/Layout.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 export default function Layout(props) {
   return (
     <>
       <div>Header for Logged In Users</div>
+      <div>
+        <Link to="/"> Home </Link> <br />
+        <Link to="/bike"> Bike </Link>
+        <br />
+        <Link to="/truck">Truck</Link>
+        <br />
+      </div>
+      <hr />
       {props.children}
       <div>Footer Users</div>
     </>
