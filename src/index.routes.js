315147ec9645f87d69b6e0c2dc73696da1d6bb14
Ehssan ElAtfy <ehssan.elatfy@reno.systems>
2023-06-11 21:42:42 +0300

    fix merge conflict

--- src/index.routes.js
+++ src/index.routes.js
@@ -3,13 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import Car from "./Car";
 import Bike from "./Bike";
 import Truck from "./Truck";
+import Layout from "./Layout";
 
 export default function RoutesList(props) {
+  const layoutify = (Component) => (
+    <Layout>
+      <Component {...props} />
+    </Layout>
+  );
+
   return (
     <Routes>
-      <Route path="/" element={<Car {...props} />} />
-      <Route path="/bike" element={<Bike {...props} />}></Route>
-      <Route path="/truck" element={<Truck {...props} />}></Route>
+      <Route path="/" element={layoutify(Car)} />
+      <Route path="/bike" element={layoutify(Bike)}></Route>
+      <Route path="/truck" element={layoutify(Truck)}></Route>
     </Routes>
   );
 }
